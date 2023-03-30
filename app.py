69de86278834cbbfa9702782c2c5d0ad129af7c8
Ethan Silvas <ethansilvas@gmail.com>
2023-03-30 14:31:27 -0700

    get basic owned pets code working

--- app.py
+++ app.py
@@ -164,6 +164,20 @@ if st.button("Display"):
 st.markdown("---")
 
 ################################################################################
+# View tokens that you own
+################################################################################
+st.markdown('## Your NFT Pets')
+
+session = st.session_state
+
+st.write(session.username)
+
+owned_pets = contract.functions.getOwnedTokens(session.username).call()
+
+for pet in owned_pets:
+    st.image(contract.functions.tokenURI(pet).call())
+
+################################################################################
 # View all tokens available for sale
 ################################################################################
 
